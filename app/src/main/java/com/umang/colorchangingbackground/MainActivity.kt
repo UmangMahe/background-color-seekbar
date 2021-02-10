4319a9d53bf872ce160755b1c138791e69d52e0f
Umang Maheshwari <umangmahe12@gmail.com>
2021-02-10 12:34:47 +0530

    Optimised Color behavior of text views

--- app/src/main/java/com/umang/colorchangingbackground/MainActivity.kt
+++ app/src/main/java/com/umang/colorchangingbackground/MainActivity.kt
@@ -1,28 +1,34 @@
 package com.umang.colorchangingbackground
 
+import android.annotation.SuppressLint
 import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.SeekBar
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    @SuppressLint("ResourceAsColor")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var redSP = 0
+
         var redEP = 0
-        var greenSP = 0
         var greenEP = 0
-        var blueSP = 0
         var blueEP = 0
         var red = 0
         var green = 0
         var blue = 0
 
+        val rColor = tvRedCount.currentTextColor
+        val gColor = tvGreenCount.currentTextColor
+        val bColor = tvBlueCount.currentTextColor
 
-        sbRed.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+
+        sbRed.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(
                 seekBar: SeekBar?,
                 progress: Int,
@@ -33,20 +39,20 @@ class MainActivity : AppCompatActivity() {
             }
 
             override fun onStartTrackingTouch(seekBar: SeekBar?) {
-                if(seekBar != null) {
-                    redSP = seekBar.progress
+                if (seekBar != null) {
+                    var redSP = seekBar.progress
                 }
             }
 
             override fun onStopTrackingTouch(seekBar: SeekBar?) {
-                if(seekBar != null) {
+                if (seekBar != null) {
                     redEP = seekBar.progress
                 }
             }
 
         })
 
-        sbGreen.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+        sbGreen.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(
                 seekBar: SeekBar?,
                 progress: Int,
@@ -57,20 +63,20 @@ class MainActivity : AppCompatActivity() {
             }
 
             override fun onStartTrackingTouch(seekBar: SeekBar?) {
-                if(seekBar != null) {
-                    greenSP = seekBar.progress
+                if (seekBar != null) {
+                    var greenSP = seekBar.progress
                 }
             }
 
             override fun onStopTrackingTouch(seekBar: SeekBar?) {
-                if(seekBar != null) {
+                if (seekBar != null) {
                     greenEP = seekBar.progress
                 }
             }
 
         })
 
-        sbBlue.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener{
+        sbBlue.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(
                 seekBar: SeekBar?,
                 progress: Int,
@@ -81,13 +87,13 @@ class MainActivity : AppCompatActivity() {
             }
 
             override fun onStartTrackingTouch(seekBar: SeekBar?) {
-                if(seekBar != null) {
-                    blueSP = seekBar.progress
+                if (seekBar != null) {
+                    var blueSP = seekBar.progress
                 }
             }
 
             override fun onStopTrackingTouch(seekBar: SeekBar?) {
-                if(seekBar != null) {
+                if (seekBar != null) {
                     blueEP = seekBar.progress
                 }
             }
@@ -96,7 +102,17 @@ class MainActivity : AppCompatActivity() {
 
         btnApply.setOnClickListener {
             constraintLayout.setBackgroundColor(Color.rgb(red, green, blue))
+            val avgColor = (redEP+greenEP+blueEP) / 3
+            if (avgColor >= 130) {
+                tvRedCount.setTextColor(Color.BLACK)
+                tvGreenCount.setTextColor(Color.BLACK)
+                tvBlueCount.setTextColor(Color.BLACK)
+            } else {
+                tvRedCount.setTextColor(rColor)
+                tvGreenCount.setTextColor(gColor)
+                tvBlueCount.setTextColor(bColor)
+            }
+
         }
     }
-
 }
